2469260c796f906f984e92511c885eecc3e2640d
Jenya Sovetkin <e.sovetkin@gmail.com>
2020-12-05 20:50:15 +0100

    preprocess data files
    check that all files are in WGS84 coordinate system (if not convert)
    
    split files on smaller chunks

--- create-tiles.sh
+++ create-tiles.sh
@@ -2,35 +2,40 @@
 # Original Source: https://github.com/mapbox/gdal-polygonize-test
 set -eu
 
-raster=$1
-xtiles=$2
-ytiles=$3
+raster="$1"
+xtiles="$2"
+ytiles="$3"
 
 # get raster bounds
-ul=($(gdalinfo $raster | grep '^Upper Left' | sed -e 's/[a-zA-Z ]*(//' -e 's/).*//' -e 's/,/ /'))
-lr=($(gdalinfo $raster | grep '^Lower Right' | sed -e 's/[a-zA-Z ]*(//' -e 's/).*//' -e 's/,/ /'))
+ul=($(gdalinfo "${raster}" \
+          | grep '^Upper Left' \
+          | sed -e 's/[a-zA-Z ]*(//' -e 's/).*//' -e 's/,/ /'))
+lr=($(gdalinfo "${raster}" \
+          | grep '^Lower Right' \
+          | sed -e 's/[a-zA-Z ]*(//' -e 's/).*//' -e 's/,/ /'))
 
-xmin=${ul[0]}
+xmin="${ul[0]}"
 xsize=$(echo "${lr[0]} - $xmin" | bc)
 ysize=$(echo "${ul[1]} - ${lr[1]}" | bc)
 
-xdif=$(echo "$xsize/$xtiles" | bc -l)
+xdif=$(echo "${xsize}/${xtiles}" | bc -l)
 
-for x in $(eval echo {0..$(($xtiles-1))}); do
-    xmax=$(echo "$xmin + $xdif" | bc)
-    ymax=${ul[1]}
-    ydif=$(echo "$ysize/$ytiles" | bc -l)
+for x in $(eval echo {0..$((xtiles-1))}); do
+    xmax=$(echo "${xmin} + ${xdif}" | bc)
+    ymax="${ul[1]}"
+    ydif=$(echo "${ysize}/${ytiles}" | bc -l)
 
     for y in $(eval echo {0..$((ytiles-1))}); do
-        ymin=$(echo "$ymax - $ydif" | bc)
+        ymin=$(echo "${ymax} - ${ydif}" | bc)
 
         # Create chunk of source raster
         gdal_translate -q \
-            -projwin $xmin $ymax $xmax $ymin \
+            -projwin "${xmin}" "${ymax}" "${xmax}" "${ymin}" \
             -of GTiff \
-            $raster ${raster%.tif}_${x}_${y}.tif
+            "${raster}" \
+            "${raster}_${x}_${y}.tif"
 
-        ymax=$ymin
+        ymax="${ymin}"
     done
-    xmin=$xmax
+    xmin="${xmax}"
 done
---
+++ preprocess.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# This converts all data to a common format, split file on smaller chunks
+
+export src_dir="data"
+export chunk_size=3000
+
+function convert_to_wgs84 {
+    file="$1"
+
+    if gdalinfo "${file}" | grep -q -F 'ELLIPSOID["WGS 84"'
+    then
+        return
+    fi
+
+    "Converting to WGS 84 coordinates"
+    gdalwarp "${file}" "${file}_converted.tif" -t_srs "+proj=longlat +ellps=WGS84"
+    mv "${file}_converted.tif" "${file}"
+}
+export -f convert_to_wgs84
+
+function split_tiles {
+    file="$1"
+
+    xsize=$(gdalinfo "${file}" | grep -F "Size is" | sed 's/Size is \(.*\),\(.*\)/\1/')
+    ysize=$(gdalinfo "${file}" | grep -F "Size is" | sed 's/Size is \(.*\),\(.*\)/\2/')
+
+    xtiles=$(echo "1 + ${xsize}/${chunk_size}" | bc)
+    ytiles=$(echo "1 + ${ysize}/${chunk_size}" | bc)
+
+    if [ "${xtiles}" -gt 1 ] || [ "${ytiles}" -gt 1 ]
+    then
+        ./create-tiles.sh "${file}" "${xtiles}" "${ytiles}" && rm "${file}"
+    fi
+}
+export -f split_tiles
+
+function preprocess {
+    file="$1"
+
+    echo -e "\nProcessing: ${file}"
+
+    # ignore file if not supported by gdal
+    if ! gdalinfo "${file}" &> /dev/null
+    then
+        echo "File is not supported by GDAL. Ignoring..."
+        return
+    fi
+
+    convert_to_wgs84 "${file}"
+    split_tiles "${file}"
+}
+export -f preprocess
+
+cp -rl "${src_dir}" "${src_dir}.bak"
+find "${src_dir}" -type f -exec bash -c 'preprocess "$0"' {} \;
