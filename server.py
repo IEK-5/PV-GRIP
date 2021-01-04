eaf9a2e85845705bdccc5f36a18a40468e0577d1
Evgenii Sovetkin <e.sovetkin@gmail.com>
2021-01-04 12:32:29 +0100

    increase gdal interface cache size

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ class InternalException(ValueError):
 """
 Initialize a global interface. This can grow quite large, because it has a cache.
 """
-interface = GDALTileInterface('data/','data/index.json')
+interface = GDALTileInterface('data/','data/index.json',9)
 logging.info(interface.print_used_las_space())
 
 def get_elevation(lat, lng, data_re):
