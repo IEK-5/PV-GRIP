d1fc6fad82907f72dfdcbe19dbe18afc3a381da7
Jenya Sovetkin <e.sovetkin@gmail.com>
2021-01-28 13:55:37 +0100

    print uptime and free on status

--- open_elevation/celery_status.py
+++ open_elevation/celery_status.py
@@ -14,6 +14,20 @@ def _call(what='active'):
     return json.loads(res.stdout.decode())
 
 
+def _uptime():
+    res = subprocess.run(['uptime'],
+                         stdout = subprocess.PIPE)
+    return res.stdout.decode()
+
+
+def _free():
+    res = subprocess.run(['free', '-h'],
+                         stdout = subprocess.PIPE)
+    return res.stdout.decode()
+
+
 def status():
     return {'active': _call('active'),
-            'scheduled': _call('scheduled')}
+            'scheduled': _call('scheduled'),
+            'uptime': _uptime(),
+            'free': _free()}
