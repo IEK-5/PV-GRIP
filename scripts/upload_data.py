8d4ff60599671d0f66c1a726b734277c3c3f4d0a
Jenya Sovetkin <e.sovetkin@gmail.com>
2021-02-28 19:39:21 +0100

    script to upload data

---
+++ scripts/upload_data.py
@@ -0,0 +1,32 @@
+#!/bin/env python3
+
+import os
+import sys
+
+from open_elevation.globals \
+    import get_SPATIAL_DATA, GIT_ROOT
+
+
+def print_usage(cmd):
+    print("""
+    %s <path>
+
+    path  a path that must be a subdirectory of data/current
+    """)
+
+
+if __name__ == '__main__':
+    if 2 != len(sys.argv):
+        print_usage(sys.argv[0])
+        sys.exit()
+
+    path = os.path.realpath(sys.argv[1])
+    data_path = os.path.join\
+        (GIT_ROOT,'data','current')
+
+    if os.path.commonprefix([path, data_path]) != data_path:
+        print_usage(sys.argv[0])
+        sys.exit()
+
+    data = get_SPATIAL_DATA()
+    data.upload(path)
