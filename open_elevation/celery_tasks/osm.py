5f03e17c8ad10771d888351dd6770f81c138309b
Jenya Sovetkin <e.sovetkin@gmail.com>
2021-03-04 20:10:23 +0100

    cleaning up

---
+++ open_elevation/celery_tasks/osm.py
@@ -0,0 +1,169 @@
+import os
+import shutil
+import geohash
+import requests
+
+import xml.etree.ElementTree as etree
+
+import celery
+
+from open_elevation.celery_tasks \
+    import CELERY_APP
+from open_elevation.cache_fn_results \
+    import cache_fn_results
+from open_elevation.celery_one_instance \
+    import one_instance
+from open_elevation.globals \
+    import PVGRIP_CONFIGS
+
+from open_elevation.utils \
+    import get_tempfile, remove_file, \
+    run_command, get_tempdir
+
+from cassandra_io.utils \
+    import bbox2hash
+
+
+def _form_query(bbox, tag):
+    bbox = tuple(bbox)
+    query_tags = ""
+    if tag:
+        query_tags = query_tags + \
+            f"""node{str(bbox)};
+            way[{str(tag)}]{str(bbox)};
+            relation[{str(tag)}]{str(bbox)};"""
+    else:
+        query_tags = f"""node{str(bbox)};
+                         way{str(bbox)};
+                         relation{str(bbox)};"""
+    return f"""
+    [out:xml];
+    (
+    {query_tags}
+
+    );
+    out center;
+    """
+
+
+@CELERY_APP.task()
+@cache_fn_results()
+@one_instance(expire = 10)
+def find_osm_data_online(bbox, tag):
+    query = _form_query(bbox, tag)
+
+    response = requests.get\
+        (PVGRIP_CONFIGS['osm']['url'],
+         params={'data':query},
+         headers={'referer': PVGRIP_CONFIGS['osm']['referer']})
+
+    ofn = get_tempfile()
+    try:
+        with open(ofn, 'w') as f:
+            f.write(response.text)
+    except Exception as e:
+        remove_file(ofn)
+        raise e
+
+    return ofn
+
+
+@CELERY_APP.task()
+@cache_fn_results()
+@one_instance(expire = 10)
+def create_rules(tag):
+    root = etree.Element('osm')
+
+    tree = etree.ElementTree(root)
+
+    type_ = etree.Element('way')
+
+    root.append(type_)
+
+    tag_name = etree.Element('tag')
+    type_.append(tag_name)
+
+    tag_name.set('k',tag)
+    tag_name.set('v','')
+
+    tag_action = etree.Element('tag')
+    type_.append(tag_action)
+
+    tag_action.set('k','_action_')
+    tag_action.set('v','draw:color=black;bcolor=black')
+
+    ofn = get_tempfile()
+    try:
+        with open(ofn, 'wb') as f:
+            tree.write(f)
+    except Exception as e:
+        remove_file(ofn)
+        raise e
+
+    return ofn
+
+
+@CELERY_APP.task()
+@cache_fn_results()
+@one_instance(expire = 10)
+def render_osm_data(osm_fn, rules_fn, box):
+    wdir = get_tempdir()
+    ofn = get_tempfile()
+    try:
+        run_command\
+            (what = \
+             ['smrender',
+              '-i', osm_fn,
+              '-o', 'output.png',
+              f"{str(box[0])}:{str(box[1])}:{str(box[2])}:{str(box[3])}",
+              '-r', rules_fn,
+             cwd = wdir)
+        os.rename(os.path.join(wdir,'output.png'), ofn)
+    finally:
+        shutil.rmtree(wdir)
+
+    return ofn
+
+
+@CELERY_APP.task()
+@cache_fn_results()
+@one_instance(expire = 10)
+def merge_osm(osm_files):
+    wdir = get_tempdir()
+    ofn = get_tempfile()
+    try:
+        run_command\
+            (what = ['osmconvert',
+                     *osm_files,
+                     '-o='+'output.osm'],
+             cwd = wdir)
+        os.rename(os.path.join(wdir,'output.osm'), ofn)
+    finally:
+        shutil.rmtree(wdir)
+
+    return ofn
+
+
+def _get_box_list(box, hash_length):
+    f = (geohash.bbox(i) \
+         for i in bbox2hash(box, hash_length))
+    return [(x['s'],x['w'],x['n'],x['e']) for x in f]
+
+
+def osm_render(box, tag = 'building', hash_length = 5):
+    box_list = _get_box_list(box = box,
+                             hash_length = hash_length)
+
+    tasks = celery.group\
+        (*[find_osm_data_online.signature\
+           (kwargs={'tag': tag, 'bbox':x}) \
+           for x in box_list])
+
+    tasks |= celery.group\
+        (*[merge_osm.signature(),
+           create_rules.si(tag)])
+
+    tasks |= render_osm_data.signature\
+        (kwargs={'box': box})
+
+    return tasks
