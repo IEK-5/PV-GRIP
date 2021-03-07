1b3630541b2060cf8d24f0179cb26e56efa0ffb3
Jenya Sovetkin <e.sovetkin@gmail.com>
2021-03-07 09:43:45 +0100

    minor: remove all dependencies from get_config script
    why? it should be runnable from a minimal possible system

--- scripts/get_config.py
+++ scripts/get_config.py
@@ -1,7 +1,26 @@
 #!/bin/env python3
+import os
 import sys
+import subprocess
+import configparser
 
-from open_elevation.globals \
-    import PVGRIP_CONFIGS
+
+def git_root():
+    res = subprocess.run(["git","rev-parse","--show-toplevel"],
+                         stdout=subprocess.PIPE).\
+                         stdout.decode().split('\n')[0]
+    return res
+
+
+def get_configs(fn):
+    config = configparser.ConfigParser()
+    config.read(fn)
+    return config
+
+
+GIT_ROOT = git_root()
+PVGRIP_CONFIGS = get_configs\
+    (os.path.join(GIT_ROOT,'configs','pvgrip.conf'))
+
 
 print(PVGRIP_CONFIGS[sys.argv[1]][sys.argv[2]])
