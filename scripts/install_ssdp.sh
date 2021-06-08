fa31025e546f2a85943bbd57b0e204aa9ae4e93b
Jenya Sovetkin <e.sovetkin@gmail.com>
2021-06-08 19:56:25 +0200

    ssdp: do not build for native architecture

--- scripts/install_ssdp.sh
+++ scripts/install_ssdp.sh
@@ -8,7 +8,7 @@ cd open_elevation/ssdp
 
 chmod 700 autogen.sh
 ./autogen.sh
-./configure CFLAGS=' -lm -ldl -O2 -pipe -fno-plt -flto -march=native ' \
+./configure CFLAGS=' -lm -ldl -O2 -pipe -fno-plt -flto -march=x86-64 ' \
             --enable-openmp
 make
 make install
