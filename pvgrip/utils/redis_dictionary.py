7e81250f3d2e20a0954f747a021a3790a511ab11
Jenya Sovetkin <e.sovetkin@gmail.com>
2021-08-27 07:02:58 +0200

    redis_dictionary: set expire time for dictionary items

--- pvgrip/utils/redis_dictionary.py
+++ pvgrip/utils/redis_dictionary.py
@@ -9,7 +9,8 @@ class Redis_Dictionary:
 
 
     def __init__(self, name, host, port, db,
-                 hash_function = float_hash):
+                 hash_function = float_hash,
+                 expire_time = 7200):
         """A distributed dictionary with redis
 
         :name: name of the hset in redis
@@ -21,34 +22,38 @@ class Redis_Dictionary:
         :db: redis db to use
 
         :hash_function: function that produces a hash for keys
+
+        :expire_time: time to expire for dictionary items
         """
         self._hash = hash_function
         self._client = redis.StrictRedis(host = host, port = port, db = db)
         self._name = name
+        self._expire = expire_time
 
 
     def __contains__(self, key):
         hkey = self._hash(key)
-        return self._client.hexists(self._name, hkey)
+        return self._client.exists(self._name + hkey)
 
 
     def __setitem__(self, key, item):
         hkey = self._hash(key)
         sitem = json.dumps(item)
-        self._client.hset(self._name, hkey, sitem)
+        self._client.set(self._name + hkey, sitem,
+                         ex = self._expire)
 
 
     def __getitem__(self, key):
         hkey = self._hash(key)
 
-        if not self._client.hexists(self._name, hkey):
+        if not self._client.exists(self._name + hkey):
             raise KeyError\
                 ("'{key}' is not in hset".format(key=key))
 
-        sitem = self._client.hget(self._name, hkey)
+        sitem = self._client.get(self._name + hkey)
         return json.loads(sitem)
 
 
     def __delitem__(self, key):
         hkey = self._hash(key)
-        self._client.hdel(self._name, hkey)
+        self._client.delete(self._name + hkey)
---
+++ pvgrip/utils/redis_dictionary_test.py
@@ -0,0 +1,40 @@
+import time
+
+from pvgrip.utils.redis_dictionary \
+    import Redis_Dictionary
+
+from pvgrip.globals \
+    import PVGRIP_CONFIGS
+
+
+def test_one():
+    d = Redis_Dictionary(name = 'test_one',
+                         host = PVGRIP_CONFIGS['redis']['ip'],
+                         port = 6379,
+                         db = 0)
+
+    assert 'one' not in d
+    d['one'] = 1
+    assert 'one' in d
+    assert d['one'] == 1
+    d['one'] = 2
+    assert d['one'] == 2
+    del d['one']
+    assert 'one' not in d
+
+    d[(1.2,'two')] = 2
+    assert (1.2,'two') in d
+    del d[(1.2,'two')]
+
+
+def test_timeout():
+    d = Redis_Dictionary(name = 'test_timeout',
+                         host = PVGRIP_CONFIGS['redis']['ip'],
+                         port = 6379,
+                         db = 0,
+                         expire_time = 5)
+
+    d['one'] = 1
+    assert 'one' in d
+    time.sleep(6)
+    assert 'one' not in d
