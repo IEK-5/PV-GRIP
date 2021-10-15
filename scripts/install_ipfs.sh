45b067a6d40f3d61e849b34029f1d698325a61b4
Jenya Sovetkin <e.sovetkin@gmail.com>
2021-10-15 11:02:11 +0200

    update ipfs ipfs_cluster

--- scripts/install_ipfs.sh
+++ scripts/install_ipfs.sh
@@ -5,18 +5,21 @@ cd /code
 ipfsdir=$(mktemp -d -p .)
 ipfsclusterdir=$(mktemp -d -p .)
 
+version_ipfs="0.10.0"
+version_ipfscluster="0.14.1"
+
 # install ipfs binary
 cd "${ipfsdir}"
-wget "https://dist.ipfs.io/go-ipfs/v0.9.0/go-ipfs_v0.9.0_linux-amd64.tar.gz"
-tar -xvzf "go-ipfs_v0.9.0_linux-amd64.tar.gz"
+wget "https://dist.ipfs.io/go-ipfs/v${version_ipfs}/go-ipfs_v${version_ipfs}_linux-amd64.tar.gz"
+tar -xvzf "go-ipfs_v${version_ipfs}_linux-amd64.tar.gz"
 cd go-ipfs
 bash install.sh
 cd /code
 
 # install ipfs-cluster-ctl
 cd "${ipfsclusterdir}"
-wget "https://dist.ipfs.io/ipfs-cluster-ctl/v0.14.0/ipfs-cluster-ctl_v0.14.0_linux-amd64.tar.gz"
-tar -xvzf "ipfs-cluster-ctl_v0.14.0_linux-amd64.tar.gz"
+wget "https://dist.ipfs.io/ipfs-cluster-ctl/v${version_ipfscluster}/ipfs-cluster-ctl_v${version_ipfscluster}_linux-amd64.tar.gz"
+tar -xvzf "ipfs-cluster-ctl_v${version_ipfscluster}_linux-amd64.tar.gz"
 install -Dm755 ipfs-cluster-ctl/ipfs-cluster-ctl /usr/local/bin/ipfs-cluster-ctl
 cd /code
 
