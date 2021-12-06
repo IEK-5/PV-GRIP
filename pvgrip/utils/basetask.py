55f80c1387673f250b95de967c4d7f597dfb54fc
Jenya Sovetkin <e.sovetkin@gmail.com>
2021-12-06 16:17:28 +0100

    retry task on BusyLoadingError, ConnectionError from redis

--- pvgrip/utils/basetask.py
+++ pvgrip/utils/basetask.py
@@ -12,9 +12,14 @@ from pvgrip.utils.timeout \
 from cassandra.cluster \
     import NoHostAvailable
 
+from redis.exceptions \
+    import BusyLoadingError, \
+    ConnectionError
+
 
 class WithRetry(celery.Task):
-    autoretry_for = (FAILED_METADATA,TASK_RUNNING,
-                     TIMEOUT, NoHostAvailable,)
+    autoretry_for = (FAILED_METADATA,TASK_RUNNING, TIMEOUT,
+                     NoHostAvailable, BusyLoadingError,
+                     ConnectionError, )
     retry_backoff = True
     max_retries = 10
