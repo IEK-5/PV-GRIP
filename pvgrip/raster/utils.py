IEK-5/PV-GRIP

commit 90a324ab65c401c8a87865f756eed60a8ca35a9e

fix: avoid using epsg:3857; breaking changes!
=What happened?

Although epsg:3857 units are meters, 1m on equator equates to ~1.56m
in Germany. I was misled by "units", and didn't pay attention to the
accuracy.

=How was it fixed?

In order to fix this, I changed the behaviour of the 'mesh_type'
argument. Now, it accepts either epsg code of the coordinate system to
use, or the 'utm' string. Now a user is more flexible sampling his/her
raster. In procedures, where it is important that things are measured
in meters (irradiance/shadow/filter/route) the mesh_type='utm' is
enforced.

=How it was tested?

To test things I went to google maps, and measured length of different
buildings using haversine formula, and measured resulting lengths in
lidar data using GIMP's measure tool (number of pixels * step).

=Are there breaking changes?

Yes, in order to make sure that old sampled data is not used (pickle
files), the order of latitude and longitude is changed. Previously it
was longitude first. Now it is latitude first. This becomes consistent
with the 'box' argument as well. In case the old pickled data is used,
the resulting raster file will be scrambled.

pvgrip/raster/utils.py
@@ -8,12 +8,8 @@ from scipy import ndimage as nd
 
 from pvgrip.raster.mesh \
     import mesh
-
-
-_T2MT = pyproj.Transformer.from_crs(4326, 3857,
-                                    always_xy=True)
-_T2LL = pyproj.Transformer.from_crs(3857, 4326,
-                                    always_xy=True)
+from pvgrip.utils.epsg \
+    import epsg2ll, ll2epsg
 
 
 def fill_missing(data, missing_value = -9999):
@@ -32,13 +28,13 @@ def fill_missing(data, missing_value = -9999):
 
 def check_box_not_too_big(box, step, mesh_type,
                           limit = 4400, max_points = 2e+7):
-    const = 1 if 'wgs84' == mesh_type else 111000
+    const = 1 if '4326' == str(mesh_type) else 111000
     if const*abs(box[2] - box[0])/step > limit \
        or const*abs(box[3] - box[1])/step > limit:
         raise RuntimeError\
             ("step in box should not be larger than %.2f" % limit)
 
-    grid = mesh(box = box, step = step, which = mesh_type)
+    grid = mesh(box = box, step = step, mesh_type = mesh_type)
     if len(grid['mesh'][0])*len(grid['mesh'][1]) \
        > max_points:
         raise RuntimeError\
@@ -96,7 +92,7 @@ def _neighbours(coords, neighbour_step):
            (('c','s','n'),('c','e','w'))]
 
     res = []
-    for lon_step, lat_step in itertools.product\
+    for lat_step, lon_step in itertools.product\
         ((0,neighbour_step[0],-neighbour_step[0]),
          (0,neighbour_step[1],-neighbour_step[1])):
         t = coords.copy()
@@ -106,22 +102,24 @@ def _neighbours(coords, neighbour_step):
     return np.concatenate(res), names
 
 
-def route_neighbours(route, azimuth_default, neighbour_step):
+def route_neighbours(route, azimuth_default,
+                     neighbour_step, epsg):
     coords = []
     for x in route:
         if 'longitude' not in x or 'latitude' not in x:
             raise RuntimeError\
                 ("longitude or latitude is missing!")
 
-        lon_met, lat_met = \
-            _T2MT.transform(x['longitude'], x['latitude'])
-
+        lat_met, lon_met = ll2epsg\
+            (lat = x['latitude'], lon = x['longitude'],
+             epsg = epsg)
         if 'azimuth' not in x:
             x['azimuth'] = azimuth_default
 
-        coords += [(lat_met, lon_met, lat_met, lon_met, x['azimuth'])]
+        coords += [(lat_met, lon_met,
+                    lat_met, lon_met, x['azimuth'])]
 
     res, names = _neighbours(np.array(coords), neighbour_step)
     res = _rotate(res[:,:2], res[:,2:4], res[:,4])
-    res = _T2LL.transform(res[:,1],res[:,0])
-    return np.transpose(res).tolist(), names
+    res = epsg2ll(lat = res[:,0],lon = res[:,1], epsg = epsg)
+    return np.transpose(res)[:,[1,0]].tolist(), names

pvgrip/route/utils.py
@@ -5,26 +5,21 @@ import pandas as pd
 from pvgrip.ssdp.utils \
     import timestr2utc_time
 
-
-_T2MT = pyproj.Transformer.from_crs(4326, 3857,
-                                    always_xy=True)
-
-
-def _convert_to_metric(lon, lat):
-    return _T2MT.transform(lon, lat)
+from pvgrip.utils.epsg \
+    import ll2epsg
 
 
 def write_locations(route, ghi_default, dhi_default, time_default,
                     azimuth_default, zenith_default, offset_default,
-                    locations_fn):
+                    locations_fn, epsg):
     with open(locations_fn, 'w') as f:
         for x in route:
             if 'longitude' not in x or 'latitude' not in x:
                 raise RuntimeError\
                     ("longitude or latitude is missing!")
 
-            lon_met, lat_met = \
-                _convert_to_metric(x['longitude'], x['latitude'])
+            lat_met, lon_met = \
+                ll2epsg(lat = x['latitude'], lon = x['longitude'], epsg = epsg)
 
             if 'dhi' not in x:
                 x['dhi'] = dhi_default
@@ -46,7 +41,7 @@ def write_locations(route, ghi_default, dhi_default, time_default,
             else:
                 x['utc_time'] = timestr2utc_time(x['timestr'])
 
-            fmt = '\t'.join(('%.12f',)*6 + ('%d\n',))
+            fmt = '\t'.join(('%.12f',)*7 + ('%d\n',))
 
             f.write(fmt %(lat_met, lon_met,
                           x['ghi'],x['dhi'],

pvgrip/ssdp/utils.py
@@ -51,15 +51,15 @@ def pickle2ssdp_topography(ifn, ofn):
 
     box = data['mesh']['raster_box']
     step = data['mesh']['step']
-    nlon = len(data['mesh']['mesh'][0])
-    nlat = len(data['mesh']['mesh'][1])
+    nlat = len(data['mesh']['mesh'][0])
+    nlon = len(data['mesh']['mesh'][1])
     a = data['raster'][::-1,:,:].reshape(nlon*nlat,order='F')
 
-    grid = (nlon, nlat)\
+    grid = (nlat, nlon)\
         + (box[0],
            box[1],
-           box[0]+step*nlon,
-           box[1]+step*nlat)
+           box[0]+step*nlat,
+           box[1]+step*nlon)
 
     with open(ofn, 'w') as f:
         for i in range(nlon*nlat):
@@ -111,7 +111,7 @@ def centre_of_box(box):
 
     :box: [lat_min,lon_min,lat_max,lon_max]
 
-    :return: (lon_centre,lat_centre)
+    :return: (lat_centre, lon_centre)
     """
-    return (box[1] + (box[3]-box[1])/2,
-            box[0] + (box[2]-box[0])/2)
+    return (box[0] + (box[2]-box[0])/2,
+            box[1] + (box[3]-box[1])/2)

pvgrip/webserver/utils.py
@@ -175,11 +175,11 @@ def _method_results(method, args):
                     ('weather/reanalysis',1632547215),
                     ('weather/reanalysis/route',1632547215),
                     ('weather/reanalysis/box',1632547215),
-                    ('route',1647003564),
-                    ('irradiance',1637566124),
-                    ('irradiance/ssdp',1647003564),
-                    ('intergrate',1647003564),
-                    ('filter/lidar_stdev',1639299472),
+                    ('route',1650884152),
+                    ('irradiance',1650884152),
+                    ('irradiance/ssdp',1650884152),
+                    ('intergrate',1650884152),
+                    ('filter/lidar_stdev',1650884152),
                     ]},
                   path_prefix_arg='method',
                   get_args_locally = False)

pvgrip/filter/calls.py
@@ -10,6 +10,8 @@ from pvgrip.raster.calls \
 from pvgrip.raster.tasks \
     import resample_from_pickle, sample_from_box, \
     sample_route_neighbour
+from pvgrip.raster.mesh \
+    import determine_epsg
 
 from pvgrip.route.calls \
     import route_rasters, save_route
@@ -22,13 +24,13 @@ from pvgrip.filter.tasks \
     import stdev, apply_filter
 
 
-@call_cache_fn_results(minage=1639299472)
+@call_cache_fn_results(minage = 1650884152)
 def lidar_stdev(filter_size, **kwargs):
     output_type = kwargs['output_type']
     step = kwargs['step']
     kwargs['step'] = kwargs['pdal_resolution']
     kwargs['output_type'] = 'pickle'
-    kwargs['mesh_type'] = 'metric'
+    kwargs['mesh_type'] = determine_epsg(kwargs['box'], 'utm')
 
     tasks = celery.group(
         sample_raster(stat='stdev', ensure_las=True, **kwargs),
@@ -45,11 +47,11 @@ def lidar_stdev(filter_size, **kwargs):
                            to_type = output_type)
 
 
-@call_cache_fn_results()
+@call_cache_fn_results(minage = 1650884152)
 def filter_raster(filter_type, filter_size, **kwargs):
     output_type = kwargs['output_type']
     kwargs['output_type'] = 'pickle'
-    kwargs['mesh_type'] = 'metric'
+    kwargs['mesh_type'] = determine_epsg(kwargs['box'], 'utm')
 
     tasks = sample_raster(**kwargs)
     tasks |= apply_filter.signature\
@@ -94,7 +96,7 @@ def _check_filtersize_boxsize(box, filter_size,
     hows = ("region_hash","month","week","date"),
     hash_length = 4,
     maxnrows = 10000)
-@call_cache_fn_results()
+@call_cache_fn_results(minage = 1650884152)
 def lidar_stdev_route(tsvfn_uploaded, filter_size,
                       neighbour_step, azimuth, **kwargs):
     _check_filtersize_boxsize(box = kwargs['box'],
@@ -102,13 +104,12 @@ def lidar_stdev_route(tsvfn_uploaded, filter_size,
                               neighbour_step = neighbour_step,
                               azimuth = azimuth)
     kwargs['ensure_las'] = True
-    kwargs['mesh_type'] = 'metric'
 
     rasters = []
     tasks = []
     for stat in ('stdev','mean','count'):
         kwargs['stat'] = stat
-        x, y = route_rasters\
+        x, y, kwargs['mesh_type'] = route_rasters\
             (tsvfn_uploaded = tsvfn_uploaded, **kwargs)
         tasks += [x]
         if not len(rasters):
@@ -125,7 +126,7 @@ def lidar_stdev_route(tsvfn_uploaded, filter_size,
                   (kwargs = {'box': x['box'],
                              'data_re': kwargs['data_re'],
                              'stat': stat,
-                             'mesh_type': 'metric',
+                             'mesh_type': kwargs['mesh_type'],
                              'step': kwargs['pdal_resolution'],
                              'pdal_resolution': kwargs['pdal_resolution'],
                              'ensure_las': kwargs['ensure_las']},
@@ -151,17 +152,15 @@ def lidar_stdev_route(tsvfn_uploaded, filter_size,
     hows = ("region_hash","month","week","date"),
     hash_length = 4,
     maxnrows = 10000)
-@call_cache_fn_results()
+@call_cache_fn_results(minage = 1650884152)
 def filter_raster_route(tsvfn_uploaded,
                         filter_type, filter_size,
                         neighbour_step, azimuth, **kwargs):
-    kwargs['mesh_type'] = 'metric'
-
     _check_filtersize_boxsize(box = kwargs['box'],
                               filter_size = filter_size,
                               neighbour_step = neighbour_step,
                               azimuth = azimuth)
-    tasks, rasters = route_rasters\
+    tasks, rasters, kwargs['mesh_type'] = route_rasters\
         (tsvfn_uploaded = tsvfn_uploaded, **kwargs)
     prefix='filter_datare{}_stat{}_{}_filtersize{}_neighbour{}_azimuth{}'\
         .format(kwargs['data_re'], kwargs['stat'],

pvgrip/filter/tasks.py
@@ -42,7 +42,7 @@ def _write_pickle(raster, mesh):
 
 
 @CELERY_APP.task(bind=True, base=WithRetry)
-@cache_fn_results(path_prefix='filter',minage=1639299472)
+@cache_fn_results(path_prefix = 'filter', minage = 1650884152)
 @one_instance(expire = 10)
 def stdev(self, fns, filter_size):
     logging.debug("stdev\n{}"\
@@ -63,7 +63,7 @@ def stdev(self, fns, filter_size):
 
 
 @CELERY_APP.task(bind=True, base=WithRetry)
-@cache_fn_results(path_prefix='filter')
+@cache_fn_results(path_prefix = 'filter', minage = 1650884152)
 @one_instance(expire = 10)
 def apply_filter(self, fn, filter_type, filter_size):
     logging.debug("apply_filter\n{}"\

pvgrip/integrate/calls.py
@@ -3,6 +3,8 @@ from pvgrip.utils.cache_fn_results \
 
 from pvgrip.raster.calls \
     import sample_raster, convert_from_to
+from pvgrip.raster.mesh \
+    import determine_epsg
 
 from pvgrip.integrate.tasks \
     import integrate_irradiance
@@ -18,15 +20,15 @@ from pvgrip.ssdp.utils \
     maxnrows = 300000,
     scale_name = 'Wh/m^2',
     scale_constant = 3600)
-@call_cache_fn_results(minage=1647003564)
+@call_cache_fn_results(minage = 1650884152)
 def ssdp_integrate(tsvfn_uploaded, albedo,
                    offset, azimuth, zenith,
                    nsky, **kwargs):
     output_type = kwargs['output_type']
     kwargs['output_type'] = 'pickle'
-    kwargs['mesh_type'] = 'metric'
+    kwargs['mesh_type'] = determine_epsg(kwargs['box'], 'utm')
 
-    lon, lat = centre_of_box(kwargs['box'])
+    lat, lon = centre_of_box(kwargs['box'])
 
     tasks = sample_raster(**kwargs)
     tasks |= integrate_irradiance.signature\

pvgrip/integrate/tasks.py
@@ -29,7 +29,7 @@ from pvgrip.integrate.utils \
 
 
 @CELERY_APP.task(bind=True, base=WithRetry)
-@cache_fn_results(minage=1647003564, path_prefix='integrate')
+@cache_fn_results(path_prefix = 'integrate', minage = 1650884152)
 @one_instance(expire = 60*10)
 def integrate_irradiance(self, ifn, times_fn,
                          lat, lon, albedo,
@@ -73,7 +73,7 @@ def integrate_irradiance(self, ifn, times_fn,
 
 
 @CELERY_APP.task(bind=True, base=WithRetry)
-@cache_fn_results(minage=1647003564, path_prefix='integrate')
+@cache_fn_results(path_prefix = 'integrate', minage = 1650884152)
 @one_instance(expire = 60*10)
 def sum_pickle(self, pickle_files):
     logging.info("sum_pickle\n{}"\

commit 18d5e80820443455e2d8fb5b9d81f5597f93202d

fix: regression in 90a324ab
the order of longitude/latitude has changed. This affected OSM only at
the check_box_not_too_big.

pvgrip/raster/utils.py
@@ -40,7 +40,7 @@ def check_box_not_too_big(box, step, mesh_type,
         raise RuntimeError\
             ("either box or resolution is too high!")
 
-    return len(grid['mesh'][0]), len(grid['mesh'][1])
+    return len(grid['mesh'][1]), len(grid['mesh'][0])
 
 
 def index2fn(x, stat, pdal_resolution, ensure_las):