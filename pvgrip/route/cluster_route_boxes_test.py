275c502c91892302201b7c55635393d5482c8293
Jenya Sovetkin <e.sovetkin@gmail.com>
2022-06-26 12:48:01 +0200

    fix: cluster_route_boxes_test

--- pvgrip/route/cluster_route_boxes_test.py
+++ pvgrip/route/cluster_route_boxes_test.py
@@ -1,8 +1,13 @@
+import pickle
+
 import numpy as np
 import pandas as pd
 
 from pvgrip.route.cluster_route_boxes \
     import get_list_rasters
+
+from pvgrip.utils.files \
+    import get_tempfile, remove_file
 
 
 def _dummy_route(N, dummy):
@@ -17,11 +22,20 @@ def do_get_list_rasters(N=100, dummy = (1,60), box=30, box_delta=2):
     route = _dummy_route(N = N, dummy = dummy)
     box = (-box,)*2 + (box,)*2
 
-    rasters = get_list_rasters(route = route, box = box,
-                               box_delta = box_delta)
+    route_fn = get_tempfile()
+    try:
+        route.to_csv(route_fn, sep = '\t', index = False)
+        rasters_fn = get_list_rasters(route_fn = route_fn,
+                                      box = box,
+                                      box_delta = box_delta)
 
-    assert N == sum([len(x['route']) for x in rasters])
-    return rasters
+        with open(rasters_fn, 'rb') as f:
+            rasters = pickle.load(f)
+
+        assert N == sum([len(x['route']) for x in rasters])
+        return rasters
+    finally:
+        remove_file(route_fn)
 
 
 def test_get_list_rasters():
@@ -29,6 +43,8 @@ def test_get_list_rasters():
 
     do_get_list_rasters(N=1000)
     do_get_list_rasters(N=1)
+    do_get_list_rasters(N=2)
+    do_get_list_rasters(N=3)
     do_get_list_rasters(box=1000)
     do_get_list_rasters(box=1)
     do_get_list_rasters(box=1e-5)
