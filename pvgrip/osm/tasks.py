e9e07a6ea96f0c5a8e0528ddbf81c919f347c5e5
Jenya Sovetkin <e.sovetkin@gmail.com>
2022-08-18 08:54:46 +0200

    smrender: include data that lies partially outside
    -a option has been added to the smrender call to render objects
    outside of the box

--- pvgrip/osm/tasks.py
+++ pvgrip/osm/tasks.py
@@ -114,7 +114,7 @@ def merge_osm(self, osm_files):
 
 
 @CELERY_APP.task(bind=True, base=WithRetry)
-@cache_fn_results(path_prefix = 'osm', minage = 1650884152)
+@cache_fn_results(path_prefix = 'osm', minage = 1660806237)
 @one_instance(expire = 10)
 def render_osm_data(self, osm_fn, rules_fn, box, width, height):
     logging.debug("render_osm_data\n{}"\
@@ -136,11 +136,12 @@ def render_osm_data(self, osm_fn, rules_fn, box, width, height):
              ['smrender',
               '-i', osm_fn,
               '-o', 'output.png',
-              f"{str(box[0])}:{str(box[1])}:{str(box[2])}:{str(box[3])}",
               '-r', rules_fn,
               '-P','%.1fx%.1f' % (width/5, height/5),
               '-d','127',
-              '-b','white'],
+              '-b','white',
+              '-a',
+              f"{str(box[0])}:{str(box[1])}:{str(box[2])}:{str(box[3])}"],
              cwd = wdir)
         os.rename(os.path.join(wdir,'output.png'), ofn)
     finally:
